95345ab2967ce6261a25d36fdbe3b8ab77a0346c
Dario De Leonardis <>
2020-04-19 18:19:09 +0200

    lezione 8

--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -10,7 +10,7 @@
                     <h2>Ask Question</h2>
                   
                     <div class="ml-auto">
-                      <a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">Back to all Question</a>
+                      <a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">Back to all Questions</a>
                     </div>
                   </div>
                 
@@ -21,20 +21,20 @@
                         @csrf
                         <div class="form-group">
                             <label for="question-title">Question Title</label>
-                            <imput type="text" name="title" id="question-title" class="form-control {{ $errors->has ('title') ? 'is-invalid' :'' }}">
+                            <input type="text" value="{{ old('title') }}" name="title" id="question-title" class="form-control {{ $errors->has ('title') ? 'is-invalid' :'' }}">
 
                              @if ($errors->has('title'))
                                 <div class="invalid-feedback">
-                                  <strong>{{ $errors-first('title') }}</strong>
+                                  <strong>{{ $errors->first('title') }}</strong>
                                 </div>
                              @endif
                         </div>
                         <div class="form-group">
                             <label for="question-body">Explain your question</label>
-                            <textarea name="body" id="question-body" rows="10" class="form-control {{ $errors->has ('body') ? 'is-invalid' :''  }}"></textarea>
+                            <textarea name="body" id="question-body" rows="10" class="form-control {{ $errors->has ('body') ? 'is-invalid' :''  }}">{{ old('body') }}</textarea>
                             @if ($errors->has('body'))
                                 <div class="invalid-feedback">
-                                  <strong>{{ $errors-first('body') }}</strong>
+                                  <strong>{{ $errors->first('body') }}</strong>
                                 </div>
                              @endif
                         </div>
